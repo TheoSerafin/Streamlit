17e38ffabf6ef5400ab3055d25fc38c6f593e09e
Théo <theosserafin@gmail.com>
2024-12-26 12:22:07 -0300

    Update Dash.py

--- Dash.py
+++ Dash.py
@@ -2,24 +2,27 @@ import plotly.express as px
 import streamlit as st
 import pandas as pd
 
-#criando a tabela
+# Carregar a tabela do arquivo Excel
 tabela = pd.read_excel(r"C:\Users\theo_\Downloads\NovaPlanilha.xlsx")
 
-#plotando o gráfico de barra
-graf_barra = px.bar(tabela,x = tabela["Dias"],y = tabela['Total'],color_discrete_sequence=['#ff6100'])
-st.plotly_chart(graf_barra)
+# Criar o gráfico de barras
+graf_barra = px.bar(tabela, x="Dias", y="Total", color_discrete_sequence=['#ff6100'])
+st.plotly_chart(graf_barra, use_container_width=True)
 
-#criando o gráfico de pizza geral
-gastos = tabela[["Transporte", "RU","Lanche","Debito","Material","Remédio"]].sum()
+# Somar os valores de cada tipo de despesa
+gastos = tabela[["Transporte", "RU", "Lanche", "Debito", "Material", "Remédio"]].sum()
+
+# Criar um DataFrame para os gastos
 gastos_df = pd.DataFrame({'Tipo de Gasto': gastos.index, 'Valores': gastos.values})
+
+# Criar o gráfico de pizza geral
 graf_pizza = px.pie(gastos_df, names='Tipo de Gasto', values='Valores', title='Distribuição dos Tipos de Gasto')
 
+# Criar o gráfico de Saldo Disponível
+saldo = px.pie(tabela, names="Dinheiro", values="Valor", title="Saldo Disponível")
 
-#criando o gráfico de Saldo Disponivel
-saldo = px.pie(tabela, names="Dinheiro", values="Valor",title="Saldo Disponivel")
-
-#plotando os gráficos de pizza
-col1 ,col2 = st.columns(2,border=True,gap="small")
+# Plotar os gráficos de pizza em colunas
+col1, col2 = st.columns(2)
 with col1:
     st.plotly_chart(graf_pizza, use_container_width=True)
 with col2:
