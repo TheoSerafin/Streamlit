46977633a9d6e981447ac99ddfa55f949d50c4d0
Théo <theosserafin@gmail.com>
2024-12-31 12:38:16 -0300

    Update aula.py

--- aula.py
+++ aula.py
@@ -2,7 +2,7 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 
-tabela = pd.read_excel(r"Dash.xlsx")
+tabela = pd.read_excel("Dash.xlsx")
 graf_barra = px.bar(tabela, x="Produtos", y="Valor", color_discrete_sequence=['#ff6100'])
 st.plotly_chart(graf_barra, use_container_width=True)
 print(tabela)
