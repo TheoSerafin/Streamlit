3d8c78165b387d59439daa6df223b8165491066e
Théo <theosserafin@gmail.com>
2025-01-03 11:58:32 -0300

    Add files via upload

---
+++ teste.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+
+#ler arquivo
+tabela = pd.read_excel(r"C:\Users\theo_\Downloads\Teste.xlsx")
+
+#plotar gráficos
+graf_barra = px.bar(tabela, x="Produto", y="Valor", color_discrete_sequence=['#ff6100'])
+st.plotly_chart(graf_barra, use_container_width=True)
